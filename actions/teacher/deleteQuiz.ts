f3163cea45c0d9230568d9502b524e659781fa42
khushal <khushal2004a@gmail.com>
2024-10-10 21:25:05 +0530

    changes in delete button

--- actions/teacher/deleteQuiz.ts
+++ actions/teacher/deleteQuiz.ts
@@ -2,23 +2,23 @@
 import { revalidatePath } from "next/cache"
 import db from "@/db"
 
-export async function deleteQuiz({id,initial,courseID,branch}:{id:number,initial:string,courseID:string,branch:string}){
-    try{
-        const status= await db.quiz.delete({
-            where:{
-                id:id,
-                courseId:courseID,
-                teacherInitial:initial,
-                branchCode:branch
-            }
-        })
-        revalidatePath('/')
-        return {
-            message:"Successfully deleted"
-        }
-    }catch(e){
-        return{
-            error:"Error during deletion"
-        }
+export async function deleteQuiz({ id, initial, courseID, branch }: { id: number, initial: string, courseID: string, branch: string }) {
+  try {
+    await db.quiz.delete({
+      where: {
+        id: id,
+        courseId: courseID,
+        teacherInitial: initial,
+        branchCode: branch
+      }
+    })
+    return {
+      message: "Successfully deleted"
     }
+  } catch (e) {
+    console.log(e);
+    return {
+      error: "Error during deletion"
+    }
+  }
 }
